b41ffef6c91c3a52b005c21a23be81924318786a
Chris Mckenzie and Simon Adcock <chrismckenzie+simonadcock@notonthehighstreet.com>
2016-03-09 09:22:00 +0000

    get dependency modules tests

---
+++ spec/getDependecyModules.spec.js
@@ -0,0 +1,39 @@
+import test from 'ava';
+import subject from '../lib/getDependencyModules';
+
+const packageDir = '../spec/fixtures';
+
+test.cb('loads modules specified in package.json', t => {
+  const promise = subject(packageDir);
+
+  t.plan(1);
+  promise.then((packageJsonModules) => {
+    const expectedModules = {
+      '../../../spec/fixtures/fakeModule': require('./fixtures/fakeModule')
+    };
+
+    t.same(packageJsonModules, expectedModules);
+    t.end();
+  });
+});
+test.cb('does not load modules specified in both package.json and blacklist', t => {
+  const blacklist = ['../../../spec/fixtures/fakeModule'];
+  const promise = subject(packageDir, blacklist);
+
+  t.plan(1);
+  promise.then((packageJsonModules) => {
+    t.same(packageJsonModules, {});
+    t.end();
+  });
+});
+test.cb('returns empty object if specified module is not found', t => {
+  const brokenPackageDir = '../spec/fixtures/broken';
+  const promise = subject(brokenPackageDir);
+
+  t.plan(1);
+  promise.then((packageJsonModules) => {
+    t.same(packageJsonModules, {});
+    t.end();
+  });
+});
+
